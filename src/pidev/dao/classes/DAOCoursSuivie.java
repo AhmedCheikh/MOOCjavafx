46b1d122c14885b52d7600b16bde27b7fb6a6e45
Ahmed <Ahmed@HaddarAhmed>
2016-02-08 17:06:19 +0100

    First Commit

---
+++ src/pidev/dao/classes/DAOCoursSuivie.java
@@ -0,0 +1,21 @@
+
+package pidev.dao.classes;
+import pidev.dao.interfaces.IDAOCoursSuivie;
+public class DAOCoursSuivie implements IDAOCoursSuivie{
+
+    @Override
+    public void commenter() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getNote() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/pidev/dao/interfaces/IDAOCoursSuivie.java
@@ -0,0 +1,8 @@
+
+package pidev.dao.interfaces;
+
+public interface IDAOCoursSuivie {
+    void commenter() ;
+    void getNote() ;
+    void getDate();
+}
