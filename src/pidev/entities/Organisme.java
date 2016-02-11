d5ae2bdf3518411167ab1c39015e5b036141302d
Rimy Jeljeli <Rimy Jeljeli@pc-rim>
2016-02-11 04:52:01 +0100

    rim comit

--- src/pidev/entities/Organisme.java
+++ src/pidev/entities/Organisme.java
@@ -6,13 +6,14 @@
 package pidev.entities;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 /**
  *
  * @author Rimy Jeljeli
  */
-public class Organisme extends Utilisateur {
+public class Organisme  {
 
     private int id;
     private String nom;
@@ -26,8 +27,18 @@ public class Organisme extends Utilisateur {
     private int cinFormateur;
     private int idUser;
 
+    public Organisme(String nom, String email, String adresse) {
+       
+        this.nom = nom;
+        this.email = email;
+        this.adresse = adresse;
+    }
+
+    
+    
+    
     public Organisme(int id, String nom, String email, String siteweb, String adresse, Number telephone, String description, File document, File logo, int cinFormateur, int idUser, String login, String password, String role) {
-        super(idUser, login, password, role);
+//        super(idUser, login, password, role);
         this.id = id;
         this.nom = nom;
         this.email = email;
@@ -123,6 +134,33 @@ public class Organisme extends Utilisateur {
     @Override
     public String toString() {
         return "Organisme{" + "id=" + id + ", nom=" + nom + ", email=" + email + ", siteweb=" + siteweb + ", adresse=" + adresse + ", telephone=" + telephone + ", description=" + description + ", document=" + document + ", logo=" + logo + ", cinFormateur=" + cinFormateur + ", idUser=" + idUser + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + this.id;
+        hash = 47 * hash + Objects.hashCode(this.email);
+        hash = 47 * hash + Objects.hashCode(this.telephone);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Organisme other = (Organisme) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        return true;
     }
     
 
