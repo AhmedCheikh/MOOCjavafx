8845c858b336f28978693774bd2a3b1433a6206c
Gumus <Gumus@172.16.217.47>
2016-02-12 10:32:22 +0100

    Fadiiiiiiiiiiiiiiiiiiiiit

---
+++ src/pidev/Controller/AfficherChapitreApprenantController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidev.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author Gumus
+ */
+public class AfficherChapitreApprenantController {
+
+    @FXML
+    public void btnexitAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void btnbackAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void btnPasserQuizAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void btnTelechargerAction(ActionEvent event) {
+
+    }
+}
