1a0ac1a196dd998c962c158e1c3509d70a648133
Rimy Jeljeli <Rimy Jeljeli@pc-rim>
2016-02-14 15:32:16 +0100

    rim comit

--- src/pidev/Controller/PreInscrireOrganismeController.java
+++ src/pidev/Controller/PreInscrireOrganismeController.java
@@ -5,6 +5,7 @@
  */
 package pidev.Controller;
 
+import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -20,6 +21,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import pidev.dao.classes.DAOOrganisme;
 import pidev.entities.*;
@@ -29,6 +31,7 @@ import pidev.entities.*;
  * @author Rimy Jeljeli
  */
 public class PreInscrireOrganismeController implements Initializable {
+
     @FXML
     private Label l1;
     @FXML
@@ -43,8 +46,12 @@ public class PreInscrireOrganismeController implements Initializable {
     private Label l6;
     @FXML
     private Label l7;
-  
-        
+
+    @FXML
+    private Image tick1;
+
+    @FXML
+    private File document;
     @FXML
     private TextField txtNom;
     @FXML
@@ -56,57 +63,56 @@ public class PreInscrireOrganismeController implements Initializable {
     @FXML
     private TextField txtPassword;
     @FXML
-    private TextField txtPassword2;
-    
-   
+    private TextField txtpassword2;
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) { }
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 
+    public void btnValiderAction(ActionEvent event) {
 
-        public void btnValiderAction(ActionEvent event){
-    
-    
         if (txtNom.getText().isEmpty()) {
-        l1.setText("Vous devez Renseigez ce champs");
-        }else{
-        l1.setText(" ");
+            l1.setText("Vous devez Renseigez ce champs");
+        } else {
+            l1.setText(" ");
         }
         if (txtAdresse.getText().isEmpty()) {
-        l2.setText(" ");
-        }else{
-        l2.setText("Vous devez Renseigez ce champs");
+            l2.setText("Vous devez Renseigez ce champs");
+
+        } else {
+            l2.setText(" ");
         }
         if (txtEmail.getText().isEmpty()) {
-        l3.setText("Vous devez Renseigez ce champs");
-        }else{
-        l3.setText(" ");
+            l3.setText("Vous devez Renseigez ce champs");
+        } else {
+            l3.setText(" ");
         }
         if (txtLogin.getText().isEmpty()) {
-        l4.setText("Vous devez Renseigez ce champs");
-        }else{
-        l4.setText(" ");
+            l4.setText("Vous devez Renseigez ce champs");
+        } else {
+            l4.setText(" ");
         }
         if (txtPassword.getText().isEmpty()) {
-        l5.setText("Vous devez Renseigez ce champs");
-        }else{
-        l5.setText(" ");
+            l5.setText("Vous devez Renseigez ce champs");
+        } else {
+            l5.setText(" ");
         }
-        if (txtPassword2.getText().isEmpty()) {
-        l6.setText("Vous devez Renseigez ce champs");
-        }else{
-        l6.setText(" ");
+        if (txtpassword2.getText().isEmpty()) {
+            l6.setText("Vous devez Renseigez ce champs");
+        } else {
+            l6.setText(" ");
         }
-        String p=txtPassword.getText();
-         if (!txtPassword2.getText().equals(p)) {
-        l6.setText("Le Mot de passe n'est pas identique!!");
-        }else{
-        l6.setText(" ");
+        String p = txtPassword.getText();
+        if (!txtpassword2.getText().equals(p)) {
+            l6.setText("Le Mot de passe n'est pas identique!!");
+        } else {
+            l6.setText(" ");
         }
-         
-                Organisme o1 = new Organisme(3,txtNom.getText(),txtLogin.getText(),txtPassword.getText(),txtEmail.getText(),null,txtAdresse.getText(),null,null,null,null,10);
-                DAOOrganisme d1 = new DAOOrganisme();
-                d1.addOrganisme(o1);
-                 
+        System.out.println(txtNom.getText());
+        Organisme o = new Organisme();
+        Organisme o1 = new Organisme(o.getId(), txtNom.getText(), txtLogin.getText(), txtPassword.getText(), txtEmail.getText(), txtAdresse.getText());
+        DAOOrganisme d1 = new DAOOrganisme();
+        d1.addOrganisme(o1);
 
 //         ((Node) (event.getSource())).getScene().getWindow().hide();
 //            Parent parent;
@@ -120,19 +126,48 @@ public class PreInscrireOrganismeController implements Initializable {
 //            stage.show();
     }
 
-  public void btnAnullerAction(ActionEvent event){
-    txtNom.setText("");
-    txtLogin.setText("");
-    txtPassword.setText("");
-    txtPassword2.setText("");
-    txtEmail.setText("");
-    txtAdresse.setText("");
+    public void btnAnullerAction(ActionEvent event) {
+        txtNom.setText("");
+        txtLogin.setText("");
+        txtAdresse.setText("");
+        txtEmail.setText("");
+        txtPassword.setText("");
+        txtpassword2.setText("");
+
     }
-     public void btnexitAction(ActionEvent event){
-    
+
+    public void btnexitAction(ActionEvent event) {
+
     }
-    public void btnbackAction(ActionEvent event){
-    
+
+    public void btnbackAction(ActionEvent event) {
+
     }
+
+    public void btnChoisireAction() {
+        
+        FileChooser fileChooser = new FileChooser();
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+
+            
+          
+            fileChooser.setTitle("Open resource file");
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.pdf"));
+            Organisme o = new Organisme();
+            if (selectedFile != null) {
+                File path = selectedFile.getAbsoluteFile();
+                l7.setText("File selected: " + selectedFile.getName());
+               o.setDocument(path);
+            }
+            else {
+
+    l7.setText("File selection cancelled.");
+
 }
 
+        }
+
+    }}
