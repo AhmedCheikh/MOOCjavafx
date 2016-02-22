e9f30f03100c186b8e7d688b508facace8c4979e
Ahmed <Ahmed@HaddarAhmed>
2016-02-22 19:36:21 +0100

    Merge origin/master
    Conflicts:
    	nbproject/UPDATED.TXT
    	nbproject/jfx-impl.xml
    	src/pidev/Controller/AffichageCoursController.java
    	src/pidev/Controller/AfficherChapitreApprenantController.java
    	src/pidev/Controller/AfficherCoursEtChapitreApprenantController.java
    	src/pidev/Controller/AjouterChapitreController.java
    	src/pidev/Controller/ControllerAthentification.java
    	src/pidev/Controller/ModifierChapitreController.java
    	src/pidev/dao/classes/DAOChapitre.java
    	src/pidev/dao/classes/DAOCours.java
    	src/pidev/dao/interfaces/IDaoCours.java
    	src/pidev/gui/FXMLAffichageCours.fxml
    	src/pidev/gui/FXMLAuthentification.fxml
    	src/pidev/gui/ModifierChapitre.fxml
    	src/pidev/gui/img/JavaFx.pdf
    	src/pidev/tests/test.java

--- src/pidev/tests/test.java
+++ src/pidev/tests/test.java
@@ -1,10 +1,5 @@
 
 package pidev.tests;
-import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import com.restfb.FacebookClient.AccessToken;
-import com.restfb.types.User;
-import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -38,13 +33,7 @@ public class test extends Application
 
     public static void main(String[] args) {
         launch(args);
-//        String accesstoken="CAAL8ZCtdpy2QBAP7PgCizW72wekEYaie28i2do1yhphgtdE4G9xVsRZBzMbOizXupksEGD8ifbodV2fBd6D7gAR0fQC3W1ayzrZAZAwYCrLrYZB4m2EABPTLYRus18hyXLaJeTuLZBnBUqLKNMPrEZAzWi2FGNdtZADdSXNry8Kz8hrqZAdAOhpptpbBzZAGCcIkoZD";
-//        FacebookClient fbClient= new DefaultFacebookClient(accesstoken);
-////        User me=fbClient.fetchObject("me", User.class);
-////        System.out.println(me.getName());
-//AccessToken exAccessToken= fbClient.obtainExtendedAccessToken("841121419348836","511eb843c98cee4e53fcb83240b27c13");
-//        System.out.println(exAccessToken.getAccessToken());
-//        System.out.println(exAccessToken.getExpires());
+
         
 
 }
